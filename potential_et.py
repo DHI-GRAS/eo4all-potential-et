1e0226cd7cc0de1b6e3505114a2ac355fdf0effc
charalamm <charalamm@protonmail.com>
2023-11-27 10:27:00 +0100

    FIx

--- potential_et.py
+++ potential_et.py
@@ -307,9 +307,11 @@ def main(aoi_name, date, spatial_res="s2", temporal_res="dekadal"):
     out_folder = Path(f"/data/outputs/{aoi_name}/{date_start:%Y%m%d}/10m/Potential-Evapotranspiration")
     out_file = out_folder / f"Potential-Evapotranspiration_ETp_S2-10m_{aoi_name}_{date_start:%Y%m%d}_{date_end:%Y%m%d}_{dt.datetime.now():%Y%m%d%H%M%S}.tif"
     out_file_existence_check = out_folder / f"Potential-Evapotranspiration_ETp_S2-10m_{aoi_name}_{date_start:%Y%m%d}_{date_end:%Y%m%d}_*.tif"
+    existing_files = glob(str(out_file_existence_check))
+    print(existing_files)
     # Check if output file already exists and return if it does
-    if os.getenv("DEBUG", None) is None and glob(str(out_file_existence_check)):
-        return str(glob(str(out_file_existence_check))[0])
+    if os.getenv("DEBUG", None) is None and existing_files:
+        return str(existing_files[0])
 
     # Download VI data
     print("Accessing VI data...")
